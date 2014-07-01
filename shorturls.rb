f53cccb3aa8ab5c2cb51a19cf076a32e79f132f3
Zach Bloomquist <zbloomq@live.com>
2014-07-01 15:12:06 -0400

    Commenting ShortUrls class

--- shorturls.rb
+++ shorturls.rb
@@ -1,12 +1,26 @@
+# This is a class which handles the creation and getting of short URLs
+# from a database.
+
 require 'sequel'
 require 'base64'
 require 'uri'
 
+# This is the main ShortUrls class, initialized with one argument: the
+# Sequel connection string.
+
 class ShortUrls
+
+	# Initialize database connection and create pointer to ShortUrls
+	# table.
 	def initialize( connection )
 		@DB = Sequel.connect( connection )
 		@urls = @DB[ :ShortUrls ]
 	end
+	
+	# Validate a URL using the uri gem and check to see if it already
+	# exists. If it does, it will not be added again; instead, the old
+	# short URL will be returned. Returns the short URL in base36
+	# string format.
 	def put( url )
 		# firstly validate the url
 		unless url =~ /\A#{URI::regexp(['http', 'https'])}\z/
@@ -20,6 +34,8 @@ class ShortUrls
 			return @urls.insert( :url => Base64.encode64( url ) ).to_s(36)
 		end
 	end
+	
+	# Get a short URL by base36 ID and return the row from the DB.
 	def get( short )
 		short_id = short.to_i(36)
 		result = @urls[ :id => short_id ]
