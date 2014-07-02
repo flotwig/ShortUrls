29bc28420a0097073ec6d2421d31a318526c6257
Zach Bloomquist <zbloomq@live.com>
2014-07-02 01:46:38 -0400

    Adding yardoc-compatible docs

--- shorturls.rb
+++ shorturls.rb
@@ -4,12 +4,12 @@
 require 'sequel'
 require 'base64'
 require 'uri'
-# This is the main ShortUrls class, initialized with one argument: the
-# Sequel connection string.
+# This is the main ShortUrls class.
 class ShortUrls
 
 	# Initialize database connection and create pointer to ShortUrls
 	# table.
+	# @param connection [String] A Sequel connection string.
 	def initialize(connection)
 		@DB = Sequel.connect(connection)
 		@urls = @DB[:ShortUrls]
@@ -18,6 +18,9 @@ class ShortUrls
 	# exists. If it does, it will not be added again; instead, the old
 	# short URL will be returned. Returns the short URL in base36
 	# string format.
+	# @param url [String] A URL to be shortened.
+	# @return [Boolean] False if URL is invalid.
+	# @return [String] The ID of the string in base 36 (a-z0-9, like 0a or 3a4zfq
 	def put(url)
 		# firstly validate the url
 		return false unless url =~ /\A#{URI.regexp(['http','https'])}\z/
@@ -27,6 +30,8 @@ class ShortUrls
 		return @urls.insert(:url => Base64.encode64(url)).to_s(36)
 	end
 	# Get a short URL by base36 ID and return the row from the DB.
+	# @param short [String] A base36 ID (a-z0-9, like 0a or 3a4zfq)
+	# @return [Hash] The row from the database. { :id, :url }
 	def get(short)
 		short_id = short.to_i(36)
 		result = @urls[:id => short_id]
