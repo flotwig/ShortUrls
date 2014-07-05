b9aa309011b0300be3f7a4836860f449f481e8ca
Zach Bloomquist <zbloomq@live.com>
2014-07-05 13:36:55 -0400

    Adding banned keyword ability

--- server.rb
+++ server.rb
@@ -17,12 +17,15 @@ config = YAML.load_file('config.yaml')
 Short = ShortUrls.new(config['db'])
 
 get '/api/new' do
-	id = Short.put(params['url'])
-	if id
+	if config['banned_keywords'].any? { |k| params['url'].include? k }
+		JSON.generate('status' => false,
+		              'msg'    => "That URL contains banned keywords.")	
+	elsif id = Short.put(params['url'])
 		JSON.generate('id'     => id,
 		              'status' => true)
 	else
-		JSON.generate('status' => false)
+		JSON.generate('status' => false,
+		              'msg'    => "That URL is invalid.")
 	end
 end
 
